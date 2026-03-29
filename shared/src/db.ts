48881797374a34d242dfacd9c0d3d053cb47ad1a
odisfm <102568741+odisfm@users.noreply.github.com>
2026-03-30 09:31:32 +1100

    remove debug logging

--- shared/src/db.ts
+++ shared/src/db.ts
@@ -1,7 +1,5 @@
 import { PrismaClient, FinalType } from './generated/prisma/client.ts'
 import { PrismaPg } from '@prisma/adapter-pg'
 
-console.log(process.env.DATABASE_URL)
-
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
 export const db = new PrismaClient({ adapter })
