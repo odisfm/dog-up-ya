7c230af4284c489d4aaa5cb034e713259fa4fc4b
odisfm <102568741+odisfm@users.noreply.github.com>
2026-03-30 21:15:11 +1100

    add routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import Ladder from "./Ladder.tsx";
+import {Outlet} from "react-router";
 
 function App() {
 
@@ -6,10 +6,10 @@ function App() {
 
     return (
         <>
-            <div className={"bg-stone-100 flex flex-col items-center justify-center flex-1 text-xl"}>
+            <div className={"bg-stone-100 flex flex-col items-center justify-center flex-1"}>
                 <h1 className={"text-purple-500 text-5xl"}>Hello</h1>
 
-                <Ladder />
+                <Outlet />
             </div>
         </>
     )
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,18 +1,21 @@
-import ReactDOM from "react-dom/client";
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
+import {BrowserRouter, Navigate, Route, Routes} from "react-router";
 import App from './App.tsx'
-import {createBrowserRouter} from "react-router";
-import {RouterProvider} from "react-router/dom";
+import Ladder from "./Ladder.tsx";
+import Round from "./Round.tsx";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App/>,
-    },
-]);
-
-const root = document.getElementById("root")!;
-
-ReactDOM.createRoot(root).render(
-    <RouterProvider router={router}/>,
-);
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App/>}>
+                    <Route index element={<Navigate to="/round" replace />} />
+                    <Route path="ladder/:season?" element={<Ladder />} />
+                    <Route path="round/:season?/:roundNum?" element={<Round />}></Route>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>,
+)
