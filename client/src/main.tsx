97a79c64c21d9bf5ed65cc8194a3f19df7c92679
odisfm <102568741+odisfm@users.noreply.github.com>
2026-03-30 12:23:02 +1100

    run formatter

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,18 +1,18 @@
 import ReactDOM from "react-dom/client";
 import './index.css'
 import App from './App.tsx'
-import { createBrowserRouter } from "react-router";
-import { RouterProvider } from "react-router/dom";
+import {createBrowserRouter} from "react-router";
+import {RouterProvider} from "react-router/dom";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <App />,
+        element: <App/>,
     },
 ]);
 
 const root = document.getElementById("root")!;
 
 ReactDOM.createRoot(root).render(
-    <RouterProvider router={router} />,
+    <RouterProvider router={router}/>,
 );
