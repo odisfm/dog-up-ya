e26e5fb73ccf78c08cc782518da52203e1984a52
odisfm <102568741+odisfm@users.noreply.github.com>
2026-03-30 12:10:51 +1100

    add api response types

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -3,4 +3,6 @@ import { resolve } from 'path';
 export * from './types/squiggle.ts'
 export { db } from './db.ts'
 export * as PrismaExports from './generated/prisma/client.ts'
+export {Prisma} from './generated/prisma/client.ts'
+export type {Season, Team, SeasonTeam, Round, Game, Standing, Tip, ScoreEvent} from './generated/prisma/client.ts'
 export * as consts from './consts.js'
---
+++ shared/src/types/apiResponses.ts
@@ -0,0 +1,21 @@
+import type { Season, Standing, Team, Round, Game, ScoreEvent } from '@footy-scores/shared/'
+import { Prisma } from '@footy-scores/shared'
+
+
+export type LadderResponse = Array<Prisma.StandingGetPayload<{
+    include: { team: true }
+}>>
+
+export type GameResponse = Prisma.GameGetPayload<{
+    include: { homeTeam: true, awayTeam: true }
+} & {
+    scoreEvents: ScoreEvent[]
+}>
+
+export type RoundResponse = Round & {
+    games: GameResponse[]
+}
+
+export type SeasonResponse = Season & {
+    games: Game[]
+}
