6104d7ca75190cb51ca47bfd67b7d95805d571a9
odisfm <102568741+odisfm@users.noreply.github.com>
2026-03-30 13:23:29 +1100

    add basic ladder

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,15 @@
+import Ladder from "./Ladder.tsx";
+
 function App() {
+
+
 
     return (
         <>
-            <div className={"bg-stone-950 flex flex-col items-center justify-center flex-1"}>
+            <div className={"bg-stone-100 flex flex-col items-center justify-center flex-1 text-xl"}>
                 <h1 className={"text-purple-500 text-5xl"}>Hello</h1>
+
+                <Ladder />
             </div>
         </>
     )
---
+++ client/src/Ladder.tsx
@@ -0,0 +1,77 @@
+import {useEffect, useState} from "react";
+import type {LadderResponse} from "@footy-scores/shared/src/types/apiResponses.ts";
+
+export default function Ladder() {
+    const [ladder, setLadder] = useState<LadderResponse | null>(null);
+    const [failed, setFailed] = useState(false);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const response = await fetch(`${import.meta.env.VITE_API_URL}/ladder`);
+                const data = await response.json();
+                if (data.data) {
+                    setLadder(data.data);
+                }
+                else {
+                    setFailed(true);
+                }
+            } catch (e) {
+                console.error(e);
+                setFailed(true);
+            }
+
+        })()
+    }, []);
+
+    return (
+        <>
+            {!failed  && !ladder &&
+            <h1>Getting ladder...</h1>
+            }
+            {failed &&
+            <h1>Failed to get ladder... :(</h1>
+            }
+
+            {ladder &&
+            <table>
+                <thead>
+                <tr className={"*:px-2 bg-sky-600 text-white"}>
+                    <th className={"px-3 text-right"}>#</th>
+                    <th className={"text-left"}>Team</th>
+                    <th>Played</th>
+                    <th>Won</th>
+                    <th>Pts</th>
+                    <th>%</th>
+                </tr>
+                </thead>
+                <tbody>
+                    {ladder.map((standing, i) => (
+                        <tr key={standing.team.id} className={"odd:bg-stone-200 *:px-2"}>
+                            <td className={"px-3 text-right"}>
+                                {i + 1}
+                            </td>
+                            <td className={"text-left"}>
+                                {standing.team.name.length < 17 ? standing.team.name : standing.team.abbreviation}
+                            </td>
+                            <td>
+                                {standing.played}
+                            </td>
+                            <td>
+                                {standing.wins}
+                            </td>
+                            <td>
+                                {standing.premPoints}
+                            </td>
+                            <td>
+                                {standing.percentage.toFixed(1)}%
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
+            }
+        </>
+    )
+}
