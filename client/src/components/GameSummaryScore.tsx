dbedd6e22db163ff1bcf32a1e7b4aa66b82dd187
odisfm <102568741+odisfm@users.noreply.github.com>
2026-04-02 15:42:31 +1100

    stop margin pill animating on/off

--- client/src/components/GameSummaryScore.tsx
+++ client/src/components/GameSummaryScore.tsx
@@ -7,13 +7,9 @@ export default function GameSummaryScore({score, margin, goals, behinds}:
         <div className={`game-summary-score h-full gap-2`}>
             <span className={"self-end"}>{`${goals}.${behinds}`}</span>
             <span className={`text-3xl md:text-4xl self-center font-bold`}>{score}</span>
-            { winning ?
-                <span className={`self-start rounded-md px-3 py-0 font-bold text-xs text-white bg-cyan-600 justify-self-center`}>
-                    {`+${margin}`}
-                </span>
-                :
-                <span></span>
-            }
+            <span className={`${margin <= 0 && `hidden`} self-start rounded-md px-3 py-0 font-bold text-xs text-white bg-cyan-600 justify-self-center`}>
+                {`+${margin}`}
+            </span>
         </div>
 
     )
