826ab779828123e441c19ec518b12f6c495bc7f5
odisfm <102568741+odisfm@users.noreply.github.com>
2026-04-02 20:07:19 +1100

    fix url

--- consumers/watchSquiggleEvents.ts
+++ consumers/watchSquiggleEvents.ts
@@ -7,8 +7,8 @@ import type {SquiggleEventGame, SquiggleEventScore, SquiggleEventTime, SquiggleE
 const args: string[] = process.argv.slice(2)
 const TEST_MODE = args.includes("test")
 
-const url = TEST_MODE ? "https://sse.squiggle.com.au/test" : "https://sse.squiggle.com.au/games";
 const es = new EventSource(url);
+const url = TEST_MODE ? "https://sse.squiggle.com.au/test" : "https://sse.squiggle.com.au/events";
 
 es.addEventListener('score', async (event) => {
     const data: SquiggleEventScore = JSON.parse(event.data);
