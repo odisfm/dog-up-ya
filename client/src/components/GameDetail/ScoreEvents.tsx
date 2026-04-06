d4b3c5b6800f891d5c7d754353455f1cf25ce3aa
odisfm <102568741+odisfm@users.noreply.github.com>
2026-04-06 16:47:15 +1000

    add warning if any score event(s) missing

--- client/src/components/GameDetail/ScoreEvents.tsx
+++ client/src/components/GameDetail/ScoreEvents.tsx
@@ -1,5 +1,6 @@
 import type {ScoreEvent, Team} from "@footy-scores/shared";
 import ScoreEventSingle from "./ScoreEventSingle.tsx";
+import {useMemo} from "react";
 
 
 export default function ScoreEvents({scoreEvents, homeTeam, awayTeam}:
@@ -9,10 +10,50 @@ export default function ScoreEvents({scoreEvents, homeTeam, awayTeam}:
         awayTeam: Team,
     }) {
 
+    const scoreEventIntegrity = useMemo(() => {
+        const orderedEvents = scoreEvents.toReversed()
+        let homeTeamRunningScore = 0
+        let awayTeamRunningScore = 0
+
+        for (const event of orderedEvents) {
+            if (event.type === "HOME_GOAL") {
+                const expectedScore = homeTeamRunningScore + 6
+                if (expectedScore !== event.hScore) {
+                    return false
+                }
+            } else if (event.type === "HOME_BEHIND") {
+                const expectedScore = homeTeamRunningScore + 1
+                if (expectedScore !== event.hScore) {
+                    return false
+                }
+            } else if (event.type === "AWAY_GOAL") {
+                const expectedScore = awayTeamRunningScore + 6
+                if (expectedScore !== event.aScore) {
+                    return false
+                }
+            } else if (event.type === "AWAY_BEHIND") {
+                const expectedScore = awayTeamRunningScore + 1
+                if (expectedScore !== event.aScore) {
+                    return false
+                }
+            }
+
+            homeTeamRunningScore = event.hScore || 0
+            awayTeamRunningScore = event.aScore || 0
+        }
+
+        return true
+
+    }, [scoreEvents])
+
     return (
         <div className={"flex flex-col gap-1 w-full"}>
             { scoreEvents.length === 0 &&
                 <span>Both teams yet to score</span>
+            }
+
+            { !scoreEventIntegrity &&
+                <span className={"mt-2 mb-3 px-4 py-1 self-center rounded-md bg-mist-700 dark:bg-mist-900"}>Some scoring shots are missing!</span>
             }
 
             { scoreEvents.map((event: ScoreEvent, i)=> {
