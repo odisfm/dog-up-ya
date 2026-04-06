69bbc606110e7b6448c9ba156dd4247f20c1de26
odisfm <102568741+odisfm@users.noreply.github.com>
2026-04-06 17:39:07 +1000

    add LinkButton

---
+++ client/src/components/LinkButton.tsx
@@ -0,0 +1,43 @@
+import {Link} from "react-router";
+import { FaExternalLinkAlt } from "react-icons/fa";
+
+export default function LinkButton({url, label, size}:
+                                   { url: string, label: string, size: "sm" | "md" | "lg"}) {
+    const commonStyles = "cursor-pointer rounded-md bg-mist-600 dark:bg-mist-800 group-hover:bg-mist-700 group-hover:dark:bg-mist-900 flex gap-2 items-center"
+    let sizeStyles = ""
+    switch (size) {
+        case "sm":
+            sizeStyles = "px-2 py-1"
+            break;
+        case "md":
+            sizeStyles = "px-3 py-2 text-lg"
+            break;
+        case "lg":
+            sizeStyles = "px-5 py-3 text-xl"
+            break;
+    }
+
+    const external = url.startsWith("https://") || url.startsWith("http://")
+
+    return (
+        <>
+            {external ? (
+                <a href={url} className={"group"} target="_blank" rel="noreferrer">
+                    <button
+                        className={`${sizeStyles} ${commonStyles}`}>
+                        {label}
+                        <FaExternalLinkAlt className={"text-sm"}/>
+
+                    </button>
+                </a>
+            ) : (
+                <Link to={url} className={"group"}>
+                    <button
+                        className={`${sizeStyles} ${commonStyles}`}>
+                        {label}
+                    </button>
+                </Link>
+            )}
+        </>
+    )
+}
