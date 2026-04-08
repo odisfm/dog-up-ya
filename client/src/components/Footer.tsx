3fe51384632fbf718ebbb2eafd4e5a1c220d8ddb
odisfm <102568741+odisfm@users.noreply.github.com>
2026-04-08 16:48:27 +1000

    add footer links

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,7 +1,21 @@
+import {FaGithub} from "react-icons/fa";
+import {MdEmail} from "react-icons/md";
+import {APP_NAME, GITHUB_REPO_LINK, MAILTO_ADDRESS} from "../consts.ts";
+
 export default function Footer() {
+    const linkStyles = `group hover:bg-mist-600 hover:dark:bg-mist-700 px-2 py-1 rounded-lg cursor-pointer`
     return (
-        <footer className={`bg-mist-500 dark:bg-mist-900 py-3 md:p-6 text-white font-light text-xs`}>
+        <footer className={`bg-mist-500 dark:bg-mist-900 grid grid-cols-3 grid-rows-1 py-3 md:p-6 text-white font-light text-xs`}>
+            <div></div>
             <span>data from <a href={"https://squiggle.com.au/"} target={"_blank"}>Squiggle</a>, site by <a href={"https://odis.fm"} target={"_blank"}>odis</a></span>
+            <div className={"flex gap-2 items-center justify-self-end text-lg"}>
+                <a className={`${linkStyles}`} href={`mailto:${MAILTO_ADDRESS}?subject=${encodeURIComponent(APP_NAME)}`} target={"_blank"}>
+                    <MdEmail />
+                </a>
+                <a className={`${linkStyles}`} href={GITHUB_REPO_LINK} target={"_blank"}>
+                    <FaGithub/>
+                </a>
+            </div>
         </footer>
     )
 }
--- client/src/consts.ts
+++ client/src/consts.ts
@@ -1,4 +1,6 @@
 export const APP_NAME = "Footy Scores"
+export const GITHUB_REPO_LINK = "https://github.com/odisfm/footy-scores-test"
+export const MAILTO_ADDRESS = "itsyaboiodis@hotmail.com"
 export const REFRESH_TIME_MS = 5000 // time to poll for live games
 export const ROUND_SEGMENT_LIVE_LABEL = "Live!"
 export const FIRST_SEASON = 1897
