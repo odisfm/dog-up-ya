614198a9052b0f4041f3fb641d5345fe2db8cde5
odisfm <102568741+odisfm@users.noreply.github.com>
2026-04-09 08:01:54 +1000

    use specific iam account with access key

--- infra/variables.tf
+++ infra/variables.tf
@@ -11,3 +11,18 @@ variable "active_deployment_target" {
     error_message = "target_group must be either 'blue' or 'green'."
   }
 }
+
+variable "aws_access_key" {
+  description = "AWS IAM access key"
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS IAM secret key"
+  sensitive   = true
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  default     = "ap-southeast-4"
+}
