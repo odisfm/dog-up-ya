538ab7b7eebb0d1470515c8d4e0227b47654817f
odisfm <102568741+odisfm@users.noreply.github.com>
2026-04-08 17:14:16 +1000

    style

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -3,12 +3,17 @@ import {MdEmail} from "react-icons/md";
 import {APP_NAME, GITHUB_REPO_LINK, MAILTO_ADDRESS} from "../consts.ts";
 
 export default function Footer() {
-    const linkStyles = `group hover:bg-mist-600 hover:dark:bg-mist-700 px-2 py-1 rounded-lg cursor-pointer`
+    const linkStyles = `group hover:bg-mist-600 hover:dark:bg-mist-700 px-1 md:px-2 py-1 rounded-lg cursor-pointer`
     return (
-        <footer className={`bg-mist-500 dark:bg-mist-900 grid grid-cols-3 grid-rows-1 py-3 md:p-6 text-white font-light text-xs`}>
-            <div></div>
-            <span>data from <a href={"https://squiggle.com.au/"} target={"_blank"}>Squiggle</a>, site by <a href={"https://odis.fm"} target={"_blank"}>odis</a></span>
-            <div className={"flex gap-2 items-center justify-self-end text-lg"}>
+        <footer className={`
+        bg-mist-500 dark:bg-mist-900 
+        grid py-3 md:p-6 px-4 md:px-12 items-center
+        text-white font-light text-xs md:text-sm`}>
+            <div className={"attr-text text-left"}>
+                <span>data from <a href={"https://squiggle.com.au/"} target={"_blank"}>Squiggle</a>,
+                    site by <a href={"https://odis.fm"} target={"_blank"}>odis</a></span>
+            </div>
+            <div className={"icons flex gap-1 items-center justify-self-end text-lg"}>
                 <a className={`${linkStyles}`} href={`mailto:${MAILTO_ADDRESS}?subject=${encodeURIComponent(APP_NAME)}`} target={"_blank"}>
                     <MdEmail />
                 </a>
