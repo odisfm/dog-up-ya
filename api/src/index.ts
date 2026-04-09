2eab74239778c1137f58c38530c738762aff99e9
odisfm <102568741+odisfm@users.noreply.github.com>
2026-04-09 13:58:32 +1000

    listen on all interfaces
    (cherry picked from commit 2c7fe790c9593905d3cb2571d92bf65be22b23b1)

--- api/src/index.ts
+++ api/src/index.ts
@@ -192,7 +192,8 @@ app.get(`/game/:gameId`, async (c) => {
 
 serve({
   fetch: app.fetch,
-  port: 3000
+  port: 3000,
+  hostname: '0.0.0.0'
 }, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
+  console.log(`Server is running on http://0.0.0.0:${info.port}`)
 })
