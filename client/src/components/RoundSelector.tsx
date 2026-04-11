e3d91adce29edaf4d4950e7741c3a62b2537a690
odisfm <102568741+odisfm@users.noreply.github.com>
2026-04-11 10:14:31 +1000

    more contextual colours

--- client/src/components/RoundSelector.tsx
+++ client/src/components/RoundSelector.tsx
@@ -72,6 +72,28 @@ export default function RoundSelector({seasonData}: {seasonData: SeasonResponse}
             <ul ref={ulRef} className={"flex gap-1 flex-nowrap flex-1 min-w-0 overflow-x-auto overflow-y-hidden text-white *:inline-block scrollbar-hide"}>
                 {roundItems.map((item) => {
                     const isActive = item.roundNumber === timeContext.round;
+                    const isCurrent = Boolean(
+                        timeContext.latestYear !== null && timeContext.latestRound !== null &&
+                        item.roundNumber === timeContext.latestRound &&
+                        timeContext.year === timeContext.latestYear && timeContext.round !== timeContext.latestRound
+                    )
+                    const isFuture = Boolean(
+                        timeContext.latestYear !== null &&
+                        timeContext.year === timeContext.latestYear &&
+                        timeContext.latestRound !== null &&
+                        item.roundNumber > timeContext.latestRound
+                    )
+                    let bgColor
+                    if (isActive) {
+                        bgColor = `bg-cyan-700`
+                    } else if (isCurrent) {
+                        bgColor = `bg-cyan-800 hover:bg-cyan-700`
+                    } else if (isFuture) {
+                        bgColor = `bg-mist-500 hover:bg-mist-600 dark:bg-mist-900 hover:dark:bg-mist-800`
+                    } else {
+                        bgColor = `bg-mist-600 hover:bg-mist-500 dark:bg-mist-950 hover:dark:bg-mist-900`
+
+                    }
                     return (
                         <li key={item.link}
                             ref={isActive ? setActiveTab : null}
@@ -79,9 +101,7 @@ export default function RoundSelector({seasonData}: {seasonData: SeasonResponse}
                             <button
                                 onClick={() => timeContext.setRound(item.roundNumber)}
                                 aria-label={item.accessibleLabel}
-                                className={`rounded-md py-1 px-2 min-w-15 inline-block whitespace-nowrap cursor-pointer 
-                                    ${!isActive ? `bg-mist-500 hover:bg-mist-600 dark:bg-mist-900 hover:dark:bg-mist-800`: 
-                                    `bg-cyan-700`}`
+                                className={`rounded-md py-1 px-2 min-w-15 inline-block whitespace-nowrap cursor-pointer ${bgColor}`
                             }
                                 aria-current={isActive}
                             >
