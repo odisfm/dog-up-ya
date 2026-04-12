c318a4800564a9aa9d7cead4c5bd59c405666325
odisfm <102568741+odisfm@users.noreply.github.com>
2026-04-13 09:14:22 +1000

    point cloudflare at API gateway

--- infra/dns.tf
+++ infra/dns.tf
@@ -1,35 +1,8 @@
-resource "aws_acm_certificate" "api" {
-  domain_name       = "api.dogupya.com"
-  validation_method = "DNS"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_acm_certificate_validation" "api" {
-  certificate_arn         = aws_acm_certificate.api.arn
-  validation_record_fqdns = [for r in cloudflare_dns_record.api_cert_validation : r.name]
-}
-
-resource "cloudflare_dns_record" "api_cert_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.api.domain_validation_options : dvo.domain_name => dvo
-  }
-
-  zone_id = var.cloudflare_zone_id
-  name    = each.value.resource_record_name
-  type    = each.value.resource_record_type
-  content = each.value.resource_record_value
-  ttl     = 1
-  proxied = false
-}
-
 resource "cloudflare_dns_record" "api" {
   zone_id = var.cloudflare_zone_id
   name    = "api"
   type    = "CNAME"
-  content = aws_lb.main.dns_name
+  content = replace(aws_apigatewayv2_api.http.api_endpoint, "https://", "")
   ttl     = 1
-  proxied = false
+  proxied = true
 }
