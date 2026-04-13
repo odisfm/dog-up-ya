ff0e122004f14d35d1327a36f0ec46efe541ad5a
odisfm <102568741+odisfm@users.noreply.github.com>
2026-04-13 10:27:53 +1000

    update dns

--- infra/dns.tf
+++ infra/dns.tf
@@ -7,6 +7,28 @@ resource "cloudflare_dns_record" "api" {
   proxied = true
 }
 
+resource "cloudflare_dns_record" "acm_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.api.domain_validation_options : dvo.domain_name => {
+      name  = dvo.resource_record_name
+      value = dvo.resource_record_value
+      type  = dvo.resource_record_type
+    }
+  }
+
+  zone_id = var.cloudflare_zone_id
+  name    = each.value.name
+  type    = each.value.type
+  content = each.value.value
+  ttl     = 60
+  proxied = false
+}
+
+resource "aws_acm_certificate_validation" "api" {
+  certificate_arn         = aws_acm_certificate.api.arn
+  validation_record_fqdns = [for record in cloudflare_dns_record.acm_validation : record.name]
+}
+
 resource "aws_acm_certificate" "api" {
   domain_name       = "api.dogupya.com"
   validation_method = "DNS"
@@ -20,6 +42,8 @@ resource "aws_apigatewayv2_domain_name" "api" {
     endpoint_type   = "REGIONAL"
     security_policy = "TLS_1_2"
   }
+
+  depends_on = [aws_acm_certificate_validation.api]
 }
 
 resource "aws_apigatewayv2_api_mapping" "api" {
