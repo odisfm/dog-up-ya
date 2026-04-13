4faba93ba9ee7764441e414088f0181dc4e3a612
odisfm <102568741+odisfm@users.noreply.github.com>
2026-04-13 10:47:04 +1000

    modify banner

--- api/esbuild.config.ts
+++ api/esbuild.config.ts
@@ -8,13 +8,6 @@ build({
     outfile: 'dist/lambda.mjs',
     format: 'esm',
     banner: {
-        js: `
-import { createRequire } from 'module';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-const require = createRequire(import.meta.url);
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-        `.trim()
-    },
+        js: `import { createRequire } from 'module'; const require = createRequire(import.meta.url);`
+    }
 })
