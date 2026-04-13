05148d630d0f93bac71f57306be93bdb460325bb
odisfm <102568741+odisfm@users.noreply.github.com>
2026-04-13 10:41:16 +1000

    add banner

--- api/esbuild.config.ts
+++ api/esbuild.config.ts
@@ -7,4 +7,14 @@ build({
     target: 'node20',
     outfile: 'dist/lambda.mjs',
     format: 'esm',
+    banner: {
+        js: `
+import { createRequire } from 'module';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const require = createRequire(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+        `.trim()
+    },
 })
